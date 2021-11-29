7fe09e85ba521bac1f71fab5a092524b380d43c4
jkugalde <jcugaldecasmail@gmail.com>
2021-11-29 09:22:47 -0300

    ajuste de tolerancias, funciones de calibracion

--- src/examples/LegTest/LegTest.ino
+++ src/examples/LegTest/LegTest.ino
@@ -1,33 +1,22 @@
 #include "LegMotor.h"
 #include "RobotLeg.h"
 
-int pinsA[3] = {3,5,A3};
-int pinsB[3] = {6,9,A4};
-
-int newpos = 0;
+int pinsA[3] = {5,3,A3};
+int pinsB[3] = {9,6,A4};
 
 RobotLeg RL1;
-
 
 void setup() {
   
   RL1.motorpins(pinsA,pinsB);
   Serial.begin(9600);
+  
+  RL1.setrefs();
 
 }
 
 void loop() {
 
- if(Serial.available()>0){
-  
- newpos = Serial.parseInt();
- Serial.println(newpos);
- 
- }
-
-Serial.print(RL1.getPos(1));
-Serial.print("    ");
-Serial.println(RL1.getPos(2));
 
   
 }
